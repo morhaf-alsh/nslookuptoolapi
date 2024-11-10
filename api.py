afa8d496cf38697fc73423c9b430e25aa606dac1
Morhaf <morhaf.alshoufi231@gmail.com>
2024-11-10 18:26:11 +0400

    uvi

--- api.py
+++ api.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from lookup import lookup
 from schemas import record_type
+import uvicorn
 
 app = FastAPI()
 
-@app.post("/dnslookup/")
+@app.get("/dnslookup/")
 async def dnslookup(record_type: record_type, request: Request):
     results = {}
     domains = await request.json()
     results = lookup(domains, record_type.value)
     return results
+
+if __name__ == '__main__':
+    uvicorn.run("api:app", port=80, host="0.0.0.0", reload=False)
