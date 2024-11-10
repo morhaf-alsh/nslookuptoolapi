2ce7ebd800677e0a772b3a19c2c4909281180af8
Morhaf <morhaf.alshoufi231@gmail.com>
2024-11-10 18:50:46 +0400

    uvi

--- api.py
+++ api.py
@@ -12,6 +12,3 @@ async def dnslookup(record_type: record_type, request: Request):
     domains = await request.json()
     results = lookup(domains, record_type.value)
     return results
-
-if __name__ == '__main__':
-    uvicorn.run("api:app", port=443, host="0.0.0.0", reload=False)
