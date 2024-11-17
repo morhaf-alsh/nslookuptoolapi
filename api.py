e3cf876a478bd409996e8b73d04a01dba94d82d1
Morhaf <morhaf.alshoufi231@gmail.com>
2024-11-17 13:07:01 +0400

    cors

--- api.py
+++ api.py
@@ -3,8 +3,21 @@ from fastapi.middleware.cors import CORSMiddleware
 from lookup import lookup
 from schemas import record_type
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/dnslookup")
 async def dnslookup(record_type: record_type, request: Request):
